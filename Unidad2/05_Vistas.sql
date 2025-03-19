ce4636eaf155520036a838a8ac506b017c0bd36b
Bruno <brunoyt530@gmail.com>
2025-03-18 19:52:40 -0600

    Logro: Funciones de cadena, vistas y store procedure terminados

---
+++ Unidad2/05_Vistas.sql
@@ -0,0 +1,115 @@
+
+ --views
+
+ -- sintaxis
+ /*create view nombreVista
+ AS
+ select columnas
+ from tabla
+ where condicion
+ */
+
+ use Northwind
+ go
+
+ create view VistaCategoriasTodas
+ AS
+ select CategoryID, CategoryName, [Description], Picture
+ from Categories;
+ go
+
+ select * from VistaCategoriasTodas
+ where CategoryName = 'Beverages'
+
+ drop view VistaCategoriasTodas
+
+ select * from Customers
+
+ -- crear una vista que permita visualizar solamente los clientes de mexico y brazil
+ go
+ create view VistaClientesLatam
+ AS
+ Select CompanyName as Clientes, Country as Pais
+ from Customers
+ where Country IN ('Mexico','Brazil')
+ go
+
+ select * from VistaClientesLatam
+ drop view VistaClientesLatam
+
+ go
+ create view VistaClientesLatam
+ AS
+ Select *
+ from Customers
+ where Country IN ('Mexico','Brazil')
+ go
+
+ select distinct vcl.Country
+ from Orders as O
+ inner join VistaClientesLatam as vcl
+ on vcl.CustomerID = o.CustomerID
+
+ -- crear una vista que contenga los datos de todas las ordenes de los productos
+ -- categorias de productos, empleados y clientes en la orden.
+ -- calcular el importe
+
+
+ create or alter view [dbo].[vistaordenescompra]
+ as
+ select o.OrderID as 'Numero de orden', o.OrderDate as 'Fecha de orden', o.RequiredDate as 'Fecha de Requesicion', 
+ CONCAT(e.FirstName, ' ', e.LastName) as 'Nombre Empleado', cu.CompanyName as Cliente, p.ProductName as 'Nombre Producto', 
+ c.CategoryName as 'Nombre de Categoria', od.UnitPrice as 'Precio de Venta', od.Quantity as 'Cantidad Vendida', 
+ (od.Quantity * od.UnitPrice) as Importe from
+ Categories as c
+ inner join Products as p
+ on c.CategoryID = p.CategoryID
+ inner join [Order Details] as od
+ on od.ProductID = p.ProductID
+ inner join Orders as o
+ on od.OrderID = o.OrderID
+ inner join Customers as cu
+ on cu.CustomerID = o.CustomerID
+ inner join Employees as e
+ on e.EmployeeID = o.EmployeeID
+
+ select count(distinct [numero orden]) as [Numero de Ordenes]
+ from vistaordenescompra
+
+ select sum([Importe] * [Precio de Venta]) as [importe total]
+ from vistaordenescompra
+ go
+
+  select sum([Importe]) as [importe total]
+ from vistaordenescompra
+ where year([Fecha de orden]) between '1995' and '1996'
+ go
+
+ select [Cliente] as 'Nombre cliente',
+ sum(Importe) as 'Importe total'
+ from vistaordenescompra
+ where year([Fecha de orden]) between '1995' and '1996'
+ group by [Cliente]
+ having count(*)>2
+ go
+
+
+ -- crear codigo rh
+ create schema rh
+
+ create table rh.tablarh (
+ id int primary key,
+ nombre nvarchar(50)
+ )
+
+
+ -- vista horizontal
+ create or alter view rh.viewcategoriasproductos
+ as
+ select c.CategoryID, c.CategoryName, p.ProductID, p.ProductName from
+ Categories as c
+ inner join Products as p
+ on c.CategoryID = p.CategoryID;
+ GO
+
+ select * from rh.viewcategoriasproductos
---
+++ Unidad2/06-transact-funciones-cadena.sql
@@ -0,0 +1,71 @@
+
+-- Funciones de cadena
+
+-- Las funciones de cadena permiten manipular tipos de datos como varchar, nvarchar, char, nchar
+
+-- funcion Len -> Devuelve la longitud de una cadena
+
+-- Declaracion de una variable
+
+DECLARE @numero int;
+set @numero = 10;
+print @numero
+
+
+-- obtener el tamaño de la cadena almacenada en la variable Texto
+
+DECLARE @Texto varchar(50) = 'Hola, mundo!'
+
+select LEN(@Texto) as Longitud
+
+-- funcion left -> Extrae un determinado numero de caracteres desde el inicio de la cadena.
+
+select LEFT(@Texto, 4) as Inicio
+
+-- funcion right -> Extrae un determinado numero de caracteres del final de la cadena
+
+select RIGHT(@Texto, 6) as Final
+
+-- substring -> Extrae una parte de la cadena
+
+select SUBSTRING(@Texto,7,5) as Extraccion
+
+-- Replace -> Reemplaza una subcadena por otra
+-- REPLACE (String_expression, string_pattern, string_replacement)
+
+select REPLACE(@Texto,'Mundo', 'Pepino') as Remplazo
+
+-- CharIndex
+
+select CHARINDEX('Mundo',@Texto) 
+
+
+DECLARE @Texto2 varchar(50) = 'Hola, mundo!'
+
+-- Upper -> Convierte una cadena en mayusculas
+select Upper(@Texto2) as Mayusculas
+select Upper(SUBSTRING(@Texto2,7,5)) as NuevoTexto
+
+select concat(left(@Texto2,5), 
+Upper(SUBSTRING(@Texto2,7,5)),
+right(@Texto2,1)) as NuevoTexto
+
+update Customers
+set CompanyName = UPPER(CompanyName)
+where country in ('Mexico', 'Germany')
+
+-- Trim -> Quita espacios en blanco de una cadena
+select Trim(  ' test ') as Result;
+
+DECLARE @Texto3 varchar(50) = 'Hola, mundo!'
+Select LTRIM(@Texto3 = '     Hola,Mundo      '
+
+select CompanyName, len(CompanyName) as 'Numero de caracteres',
+left(CompanyName, 4) as Inicio,
+right(CompanyName, 6) as Final,
+substring(CompanyName,7,4) as Extraccion
+from Customers
+
+
+
+
